c7141782230e32c662f4d66e4144db5d63668c90
andylee830914 <andylee0914.tw@gmail.com>
2014-05-26 18:22:13 +0800

    for reveal.js use

---
+++ admin/reveal/index.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+<?php
+    header('Content-Type: text/html; charset=utf-8');
+    include("../../connect.php");
+$sql="SELECT * FROM examdata";
+$result=mysql_query($sql);
+
+?>
+	<head>
+		<meta charset="utf-8">
+
+		<title>iRS 隨堂小測驗</title>
+
+		<meta name="description" content="A framework for easily creating beautiful presentations using HTML">
+		<meta name="author" content="Hakim El Hattab">
+
+		<meta name="apple-mobile-web-app-capable" content="yes" />
+		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<link rel="stylesheet" href="css/reveal.min.css">
+		<link rel="stylesheet" href="css/theme/default.css" id="theme">
+
+		<!-- For syntax highlighting -->
+		<link rel="stylesheet" href="lib/css/zenburn.css">
+
+		<!-- If the query includes 'print-pdf', include the PDF print sheet -->
+		<script>
+			if( window.location.search.match( /print-pdf/gi ) ) {
+				var link = document.createElement( 'link' );
+				link.rel = 'stylesheet';
+				link.type = 'text/css';
+				link.href = 'css/print/pdf.css';
+				document.getElementsByTagName( 'head' )[0].appendChild( link );
+			}
+		</script>
+        
+
+		<!--[if lt IE 9]>
+		<script src="lib/js/html5shiv.js"></script>
+		<![endif]-->
+	</head>
+
+	<body>
+
+		<div class="reveal">
+
+			<!-- Any section element inside of this container is displayed as a slide -->
+			<div class="slides">
+				<section>
+					<h1>隨堂小測驗</h1>
+					
+                    
+				</section>
+                <section>
+                    <img src="chart.png" align="center">
+            <p>拿出手機掃描 iRS 的 QR Code！</p>
+                    <p>可登入自己的 facebook ，讓 iRS 自動填入姓名</p>
+                </section>
+                    <?php                  
+while( $row = mysql_fetch_array($result) ){
+    if(!$row[0]==0){
+echo "<section><section><h2>第 ".$row['form']." 題</h2>";
+echo "<p>".$row['data']."</p>";
+echo '<br>';
+        if(empty($row[pic])){
+        }else{
+         echo '<img src="/irs/data/'.$row['pic'].'" style="margin:0px 30px">';
+        }
+echo '<ol style="A">';
+for ($i=0;$i<6;$i++){
+if (empty($row['choice'.$i])){
+}else{
+echo "<li>".$row['choice'.$i]."</li>";
+}
+}
+//echo '<aside class="notes">(';
+//echo $row['answer'].")</aside>";
+echo "</section><section><h2>答案是</h2><center><h1>".$row['answer']."</h1></center></section></section>";
+}
+}
+?>
+                <section>
+                    <h2>你答對了嗎？</h2>
+                    </section>
+
+				
+				
+
+			</div>
+
+		</div>
+
+		<script src="lib/js/head.min.js"></script>
+		<script src="js/reveal.min.js"></script>
+
+		<script>
+
+			// Full list of configuration options available here:
+			// https://github.com/hakimel/reveal.js#configuration
+			Reveal.initialize({
+				progress: true,
+				history: true,
+                transition: 'linear',
+                loop: true,
+                math: {
+                    //mathjax: 'http://cdn.mathjax.org/mathjax/latest/MathJax.js',
+                    config: 'TeX-AMS_HTML-full'  // See http://docs.mathjax.org/en/latest/config-files.html
+                },
+				// Parallax scrolling
+				// parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+				// parallaxBackgroundSize: '2100px 900px',
+				// Optional libraries used to extend on reveal.js
+				dependencies: [
+
+					{ src: 'plugin/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
+                    { src: 'plugin/math/math.js', async: true }
+				]
+			});
+
+		</script>
+	</body>
+</html>
